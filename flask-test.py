4f0aa073b330b7ef806211c9904a83b9ad73d6eb
Harshiacchu <harshiacchu14@gmail.com>
2022-04-02 22:13:17 +0530

    changed

--- flask-test.py
+++ flask-test.py
@@ -1,25 +1,9 @@
-'''
-Created on 
-
-Course work: 
-
-@author: Harshitha
-
-Source:
-    
-'''
-# Import necessary modules
-
 from flask import Flask
-
-
 app = Flask(__name__)
 
 @app.route('/')
-def start():
+def hello_world():
+    return 'hello world'
 
-    return 'Hello world'
-
-
-if __name__== "__main__":
-    app.run(host="0.0.0.0", debug = True)
+if __name__=='__main__':
+    app.run(debug=True)
