3f803021f2384ef3967a2974dbc792ecce82092b
Harshitha S <100066470+Harshiacchu@users.noreply.github.com>
2023-08-21 17:37:30 +0530

    Update Repeated_Substring_Pattern.py

--- Repeated_Substring_Pattern.py
+++ Repeated_Substring_Pattern.py
@@ -4,3 +4,11 @@ class Solution:
         s_fold = "".join( (s[1:], s[:-1]) )
         
         return s in s_fold
+# or #Leetcode: https://leetcode.com/problems/repeated-substring-pattern/
+# class Solution(object):
+#     def repeatedSubstringPattern(self, s):
+#         """
+#         :type s: str
+#         :rtype: bool
+#         """
+#         return s in (s+s)[1:-1]
