ee85c25357e7a3c60bb4888af2e56eecc9b6509e
Harshitha S <100066470+Harshiacchu@users.noreply.github.com>
2023-08-21 17:36:57 +0530

    Delete Repeated_Substring_pattern.py

--- Repeated_Substring_pattern.py
+++
@@ -1,8 +0,0 @@
-#Leetcode: https://leetcode.com/problems/repeated-substring-pattern/
-class Solution(object):
-    def repeatedSubstringPattern(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        return s in (s+s)[1:-1]
